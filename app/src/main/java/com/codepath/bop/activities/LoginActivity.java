ab0b4e6fb615d732c5283a380f1eb37a3fb2adff
FatemaNeemuch <fatemaneemuchwala@gmail.com>
2021-07-16 16:54:10 -0500

    clickable tvSignUp instead of btn

--- app/src/main/java/com/codepath/bop/activities/LoginActivity.java
+++ app/src/main/java/com/codepath/bop/activities/LoginActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.bop.R;
@@ -17,6 +18,8 @@ import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -27,7 +30,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
-    private Button btnSignUp;
+    private TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +46,7 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsernameWrite);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
-        btnSignUp = findViewById(R.id.btnSignUp);
+        tvSignUp = findViewById(R.id.tvSignUp);
 
         //login button
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -58,7 +61,7 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         //sign up button
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showSignUpDialog();
